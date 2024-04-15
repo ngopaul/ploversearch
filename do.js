4bdaaf18a6bf6f4ae1a144244987eda1bfbd7c47
Paul Ngo <paulngo123@gmail.com>
2024-04-15 14:31:10 -0700

    solve issues: frb = rv, special * handling, hash-change re-search, translation aware highlighting

--- do.js
+++ do.js
@@ -1,13 +1,21 @@
-function start() {
-    $('#status').text('JQuery works!  Downloading dictionary...');
-    $.getJSON("dict.json", function (dict) {
+var rot = {};
+var count_recursive_calls = 0;
+var best_hint_so_far = "";
+var start_time = 0;
+var end_time = 0;
+var codes_used = {};
+
+function loadDict(dict) {
+    $.getJSON(dict, function (dict) {
         $('#status').text('Parsing dictionary...');
-        var rot = {};
         // {
         //  'ras': { 'Raspberry': [ 'R-B', 'R*B'] },
         //  'pon': { 'ponies': [ 'P' ], 'pony': [ 'P-P' ] }
         // }
         $.each(dict, function (key, val) {
+            if (codes_used[key])
+                return true;
+            codes_used[key] = true;
             if (val.length < 2)
                 return true;
             var pre = summarise(val);
@@ -18,20 +26,40 @@ function start() {
             rot[pre][val].push(key);
         });
         delete dict;
-        $('#search').keyup(function () {
-            delay(function () {
-                doSearch(rot);
-            }, 200);
-        });
-        resultTable();
-        if (window.location.hash) {
-            $('#search').val(window.location.hash.substring(1));
-            doSearch(rot);
-        }
-        $('#search').show();
     }).fail(function (xhr, stat, error) {
         $('#status').text('Fatal error downloading dictionary.  Nothing more to see here...' + stat + " / " + error);
     });
+}
+
+function start() {
+    $('#status').text('JQuery works!  Downloading dictionary...');
+    // load dictionary_order.json to get the list of dictionaries we need to load
+    $.getJSON("dictionary_order.json", function (dicts) {
+        for (var i = 0; i < dicts.length; i++) {
+            loadDict(dicts[i]);
+        }
+    }).fail(function (xhr, stat, error) {
+        $('#status').text('Fatal error downloading dictionary_order.json.  Nothing more to see here...' + stat + " / " + error);
+    });
+
+    $('#search').keyup(function () {
+        delay(function () {
+            doSearch(rot);
+        }, 200);
+    });
+
+    // allow the user to directly link to a search term
+    window.addEventListener('hashchange', function() {
+        $('#search').val(window.location.hash.substring(1));
+        doSearch(rot);
+    }, false);
+
+    resultTable();
+    if (window.location.hash) {
+        $('#search').val(window.location.hash.substring(1));
+        doSearch(rot);
+    }
+    $('#search').show();
 }
 
 function startsWith(haystack, needle) {
@@ -60,6 +88,7 @@ function resultTable() {
 }
 
 function doSearch(dict) {
+    start_time = new Date().getTime();
     term = $('#search').val();
     if (term == prev) {
         return;
@@ -109,6 +138,9 @@ function doSearch(dict) {
         else
             inex.text('(no ' + (hadExact ? 'further ' : '') + 'matches)');
     }, 0);
+    end_time = new Date().getTime();
+    console.log("Total time: " + (end_time - start_time) + "ms");
+    // $('#time_taken').text("Time taken: " + (end_time - start_time) + "ms");
 }
 
 function addMatchesToTable(dict, r, reject) {
@@ -131,13 +163,18 @@ function addMatchesToTable(dict, r, reject) {
 function addTr(r, code, trans) {
     var cell = $('<td>');
     var splt = code.split(/\//);
+    var decomposition_hint_so_far = "";
     $.each(splt, function (idx, val) {
         var tab = $('<table>');
         var first = $('<tr>');
         var second = $('<tr>');
         tab.append(first);
         tab.append(second);
-        $.each(decompose(val), function (inner, part) {
+        let decomposition_and_hint = decompose(val, trans, decomposition_hint_so_far);
+        let decomposition = decomposition_and_hint[0];
+        let best_hint = decomposition_and_hint[1];
+        decomposition_hint_so_far += best_hint;
+        $.each(decomposition, function (inner, part) {
             var td = $('<td>');
             if (part.strokes.length <= 1)
                 td.text(part.strokes)
@@ -179,6 +216,7 @@ var meanings = [
     {from: "TPH", to: "n"},
     {from: "BGS", to: "x"},
     {from: "FPL", to: "sm"},
+    {from: "FRB", to: "rv"},
     {from: "KHR", to: "cl"}, // not ch-r
     {from: "PHR", to: "pl"}, // not m-r
     {from: "SR", to: "v"},
@@ -213,30 +251,194 @@ var meanings = [
     {from: "-", to: ""}
 ];
 
-function decompose(code) {
-    if ('' == code)
-        return [];
-    var ret;
-    for (var i = 0; i < meanings.length; ++i) {
-        var en = meanings[i];
-        var end;
-        if (startsWith(code, en.from))
-            end = en.from.length;
-        else if (startsWith(code.replace('*', ''), en.from))
-            end = en.from.length + 1;
-        else
-            continue;
+/*
+Calculate the phonetic similarity between two strings, a and b, weighting towards if a is a substring of b or vice versa.
+This is a rough approximation and should be replaced with a phonetic algorithm.
+Args:
+  a: the first string
+  b: the second string
+Returns: a number between 0 and 1, where 1 means the strings are identical and 0 means they are completely different
+ */
+function calculate_similarity(a, b) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
 
-        return [{
-            strokes: code.substring(0, end),
-            hint: en.to
-        }].concat(decompose(code.substring(end)));
+    let ratio1 = b.includes(a) || a.includes(b) ? 1 : 0;
+
+    let s = new difflib.SequenceMatcher(null, a, b);
+    let ratio2 = s.ratio();
+
+    // Get some tricky cases, such as "District of Columbia".
+    // this is a rough approximation but should be replaced with a phonetic algorithm
+    // replace all y with i
+    // replace all g with c
+    // replace all k with c
+    // replace all b with n
+    // replace ohn with on
+    let s2 = new difflib.SequenceMatcher(
+      null,
+      a.replace(/y/g, 'i').replace(/g/g, 'c').
+      replace(/k/g, 'c').replace(/b/g, 'n').
+      replace(/ohn/g, 'on'),
+      b.replace(/y/g, 'i').replace(/g/g, 'c').
+      replace(/k/g, 'c').replace(/b/g, 'n').
+      replace(/ohn/g, 'on')
+    );
+    let ratio3 = s2.ratio() * 0.9;
+
+    return Math.max(ratio1, ratio2, ratio3);
+}
+
+/*
+Handle base cases for single-letter codes.
+This allows us to substitute f, v, or s for "F"
+This allows us to substitute g, or -ing for "G"
+It also allows us to substitute special sounds, like c or k for "G", if the code contains an asterisk
+Args:
+  x: the single letter code
+  asterisk: whether the whole code contains an asterisk
+Returns: a list of possible meanings of the code
+ */
+function meanings_single_letter(x, asterisk=false) {
+    if (x === 'F' || x === 'f') {
+        return [[{from: 'F', to: 'f'}], [{from: 'F', to: 'v'}], [{from: 'F', to: 's'}]];
     }
-    var x = code.substring(0, 1);
-    return [{
-        strokes: x,
-        hint: x.toLowerCase()
-    }].concat(decompose(code.substring(1)));
+    if (x === 'G' || x === 'g') {
+        let possible_return = [[{from: 'G', to: 'g'}], [{from: 'G', to: 'ing'}]];
+        if (asterisk) {
+            possible_return.push([{from: 'G', to: 'c'}]);
+            possible_return.push([{from: 'G', to: 'k'}]);
+        }
+        return possible_return;
+    }
+    return [[{from: x, to: x.toLowerCase()}]];
+}
+
+/*
+Return all possible decompositions of the given code, in a list of
+objects, each with a 'strokes' and 'hint' property.
+Args:
+  code: the code to decompose
+  previous_hint: the hint so far, which should somewhat match the translation
+  trans: the translation to match
+  had_asterisk: whether the code contains an asterisk - this allows for some modifications to the decomposition
+    (e.g. replacing G with C or K)
+Returns: a list of all possible decompositions of the code
+  a decomposition is a list of {} objects, each with a 'strokes' and 'hint' property
+*/
+function decompose_helper(code, previous_hint, trans, had_asterisk) {
+    count_recursive_calls++;
+    if ('' === code) return [];
+
+    let possible_matches = [];
+    let possible_ends = [];
+
+    // add matches in the known hints
+    for (let i = 0; i < meanings.length; ++i) {
+        if (startsWith(code, meanings[i].from)) {
+            possible_matches.push(meanings[i]);
+            if (possible_matches[possible_matches.length - 1].to === undefined) {
+                console.log("No hint for meaning: " + meanings[i] + " in code: " + code + " and prevhint: " + previous_hint);
+            }
+            possible_ends.push(meanings[i].from.length);
+        } else if (startsWith(code.replace('*', ''), meanings[i].from)) {
+            possible_matches.push(meanings[i]);
+            if (!possible_matches[possible_matches.length - 1].to === undefined) {
+                console.log("No hint for meaning: " + meanings[i] + " in code: " + code + " and prevhint: " + previous_hint);
+            }
+            possible_ends.push(meanings[i].from.length + 1);
+        }
+    }
+
+    // add single character matches
+    let x = code.substring(0, 1);
+    let base_cases = meanings_single_letter(x, asterisk=had_asterisk);
+    for (let i = 0; i < base_cases.length; ++i) {
+        possible_matches.push(base_cases[i][0]);
+        possible_ends.push(1);
+    }
+
+    // sort possible_matches alongside possible_ends by how closely it matches the translation
+    // in the future, this can be used to prune the search space (not implemented yet)
+    // first, make some robust tests to see if this is a good idea
+    // let zipped = [];
+    // for (let i = 0; i < possible_matches.length; i++) {
+    //     zipped.push([possible_matches[i], possible_ends[i]]);
+    // }
+    // zipped.sort(function(a, b) {
+    //     let hint1 = a[0].to;
+    //     let hint2 = b[0].to;
+    //     return calculate_similarity(previous_hint + hint1, trans) -
+    //       calculate_similarity(previous_hint + hint2, trans);
+    // })
+    // possible_matches = [];
+    // possible_ends = [];
+    // for (let i = 0; i < zipped.length; i++) {
+    //     possible_matches.push(zipped[i][0]);
+    //     possible_ends.push(zipped[i][1]);
+    // }
+
+    let ret = [];
+    for (let i = 0; i < possible_matches.length; ++i) {
+        let meaning = possible_matches[i];
+        let hint = meaning.to;
+        if (hint === undefined) {
+            console.log("No hint for meaning: " + meaning + " in code: " + code + " and prevhint: " + previous_hint);
+        }
+        let end = possible_ends[i];
+        let subcalls = decompose_helper(code.substring(end), previous_hint + hint, trans, had_asterisk);
+        // base case
+        if (subcalls.length === 0) {
+            ret.push([{
+                strokes: code.substring(0, end),
+                hint: hint
+            }]);
+        }
+        for (let j = 0; j < subcalls.length; ++j) {
+            let subcall = subcalls[j];
+            let item_to_push = [{
+                strokes: code.substring(0, end),
+                hint: hint
+            }];
+            ret.push(item_to_push.concat(subcall));
+        }
+    }
+    return ret;
+}
+
+/*
+Decompose the given code into its parts, using decompose_helper to
+get all possible decompositions of the code.  Choose the best decomposition
+that matches the translation trans.
+Some tough ones: "District of Columbia", "things", "nevertheless", TODO: "Pony Express", "raspberries"
+ */
+function decompose(code, trans, decomposition_hint_so_far) {
+    count_recursive_calls = 0;
+    best_hint_so_far = "";
+    if ('' === code) return [];
+    let decompositions = decompose_helper(code, decomposition_hint_so_far, trans, code.includes('*'));
+    let best = 0;
+    let best_score = 0;
+    let best_hint = "";
+    let complete_hints = [];
+    for (let i = 0; i < decompositions.length; ++i) {
+        // concatenate all the hints of the decomposition together
+        var part_complete_hint = "";
+        for (let j = 0; j < decompositions[i].length; ++j) {
+            part_complete_hint += decompositions[i][j].hint;
+        }
+        complete_hints.push(decomposition_hint_so_far + part_complete_hint);
+        let score = calculate_similarity(decomposition_hint_so_far + part_complete_hint, trans);
+        if (score > best_score) {
+            best = i;
+            best_score = score;
+            best_hint = part_complete_hint;
+        }
+    }
+    // console.log("Total hints: " + complete_hints.join(", ") + " for code: " + code + " and translation: " + trans);
+    // console.log("Best hint: " + best_hint);
+    // console.log("Recursive calls for best-hint selection: " + count_recursive_calls);
+    return [decompositions[best], best_hint];
 }
 
 function rainbow(numOfSteps, step) {
